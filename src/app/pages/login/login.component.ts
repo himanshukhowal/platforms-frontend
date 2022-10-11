196e9cca280f993dd70d4f95771bc6167e06b132
Himanshu Khowal <khowalhimanshu1@gmail.com>
2022-10-11 06:24:59 +0530

    added toast on login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -19,7 +20,8 @@ export class LoginComponent implements OnInit, OnDestroy {
     private authenticationService: AuthenticationService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit() {
@@ -46,15 +48,34 @@ export class LoginComponent implements OnInit, OnDestroy {
     if (this.signinForm.invalid) {
       return;
     }
-    
-    this.authenticationService.login(this.signinForm.value.username, this.signinForm.value.password).subscribe(
-      data => {
+
+    this.authenticationService.login(this.signinForm.value.username, this.signinForm.value.password).subscribe({
+      next: (data) => {
         this.router.navigate([this.returnUrl]);
-      });
+      },
+      error: (err) => {
+        this.showError();
+      }
+    });
+
   }
 
   openSignup() {
     this.router.navigate(["register"]);
+  }
+
+  showError() {
+    this.toastr.error(
+      'Invalid username or password',
+      '',
+      {
+        timeOut: 8000,
+        closeButton: true,
+        tapToDismiss: false,
+        positionClass: 'toast-top-right',
+        progressBar: true
+      }
+    );
   }
 
   ngOnDestroy() {
